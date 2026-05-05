fd587dde86b1f079eabc981e638e3b89b53c8d0c
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-05-06 00:14:29 +0300

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -57,12 +57,15 @@ function App() {
       {/* === HEADER === */}
       {/* FIRST SECTION: HERO */}
       <div className="hero-section">
+        {/* VIDEO BACKGROUND */}
 
         <video autoPlay loop muted playsInline className="circuit-video-bg">
           <source src="../public/background4.mp4" type="video/mp4" />
         </video>
 
         <div className="video-overlay" />
+
+        {/* === VIDEO BACKGROUND === */}
 
         <Container className="hero-text" maxWidth="lg">
           <h1 className="hero-h1">
