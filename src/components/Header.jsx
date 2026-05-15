3d4e7f7bb484b46ef9c7bc038ca014e7d75128d2
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-05-15 23:26:44 +0300

    added responsitivity and improvments to header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,34 @@
 import "./Header.css";
+import "../App.css";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [showHeader, setShowHeader] = useState(true);
+
+  // HEADER SHOW/HIDE ANIMATION
+  useEffect(() => {
+    let lastScroll = window.scrollY;
+
+    const handleScroll = () => {
+      if (window.scrollY > lastScroll) {
+        setShowHeader(false);
+      } else {
+        setShowHeader(true);
+      }
+
+      lastScroll = window.scrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  // === HEADER SHOW/HIDE ANIMATION ===
+
   return (
     <>
-      <header>
+      <header className={showHeader ? "header-show" : "header-hide"}>
         <nav>
           {/* LOGO */}
           <div className="logo-container">
@@ -14,37 +39,51 @@ export default function Header() {
           </div>
           {/* === LOGO === */}
 
+          {/* BURGER */}
+          <div
+            className={`burger ${menuOpen ? "burger-open" : ""}`}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          {/* === BURGER === */}
+
           {/* HEADER HYPERLINKS */}
-          <ul className="nav-list">
+          <ul className={`nav-list ${menuOpen ? "nav-open" : ""}`}>
             <li className="nav-list-item">
               <a href="#about-section" className="nav-list-link">
                 About
               </a>
             </li>
+
             <li className="nav-list-item">
               <a href="#experience-section" className="nav-list-link">
                 Experience
               </a>
             </li>
+
             <li className="nav-list-item">
               <a href="#skills-section" className="nav-list-link">
                 Skills
               </a>
             </li>
+
             <li className="nav-list-item">
               <a href="#projects-section" className="nav-list-link">
                 Projects
               </a>
             </li>
+
             <li className="nav-list-item">
               <a href="#contact-section" className="nav-list-link">
                 Contact
               </a>
             </li>
           </ul>
+          {/* === HEADER HYPERLINKS === */}
         </nav>
-        {/* === HEADER HYPERLINKS === */}
-
       </header>
     </>
   );
