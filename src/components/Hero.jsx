d19c84aa465d49e5d503a62e74b4aa13ba6b6628
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-05-15 23:05:36 +0300

    added comments to hero section for clarity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,38 @@
 import "./Hero.css";
 
+// ANIMATIONS
 import TypingAnimation from "../animations/TypingAnimation";
 
+// MATERIAL UI ICONS
 import DescriptionIcon from "@mui/icons-material/Description";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 
+// MATERIAL UI COMPONENTS
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 
 export default function Hero() {
   return (
       <div id="hero-section" className="hero-section">
-        {/* VIDEO BACKGROUND */}
 
+        {/* VIDEO BACKGROUND */}
         <video autoPlay loop muted playsInline className="circuit-video-bg">
           <source src="/background1.mp4" type="video/mp4" />
         </video>
 
         <div className="video-overlay" />
-
         {/* === VIDEO BACKGROUND === */}
 
         <Container className="hero-text" maxWidth="lg">
+          {/* HERO TEXT */}
           <h1 className="hero-h1">
             From <span className="circuit-span">Circuits</span> to{" "}
             <TypingAnimation text="<Code/>" />
           </h1>
           <h2 className="hero-h2">I Engineer Full-Stack Experiences</h2>
+          {/* === HERO TEXT === */}
+
+          {/* HERO BUTTONS */}
           <Button
             component="a"
             href="./Amany Elsayed (Eg).pdf"
@@ -47,6 +53,7 @@ export default function Hero() {
           >
             Contact
           </Button>
+          {/* === HERO BUTTONS === */}
         </Container>
       </div>
   )
