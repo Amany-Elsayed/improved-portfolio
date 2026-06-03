22f3e46109c9e75b6d8c3a12ca70063011b9dae7
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-06-03 15:02:32 +0300

    removed prototype

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -73,7 +73,6 @@ export default function Hero() {
           sx={ButtonSx}
           startIcon={<AlternateEmailIcon />}
         >
-
           Contact
         </Button>
         {/* === HERO BUTTONS === */}
